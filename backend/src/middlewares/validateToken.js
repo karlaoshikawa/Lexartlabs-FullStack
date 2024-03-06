69ea6e108af4f3edc574fd6184a434f5d5fa1229
Karla Oshikawa <karla_hara@hotmail.com>
2024-03-07 08:50:11 +0900

    feat: validate token middleware

--- backend/src/middlewares/validateToken.js
+++ backend/src/middlewares/validateToken.js
@@ -3,13 +3,14 @@ const { verifyToken } = require("../auth/authToken");
 module.exports = (req, res, next) => {
   try {
     const { authorization } = req.headers;
+    console.log(verifyToken(authorization));
 
     if (!authorization) {
       return res.status(401).json({ message: "Token not found" });
     }
 
     const { payload } = verifyToken(authorization);
-
+  console.log("2", payload);
     req.payload = payload;
     next();
   } catch (err) {
--- backend/src/routes/product.router.js
+++ backend/src/routes/product.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const productController = require("../controller/product.controller");
 const productRouter = express.Router();
+const verifyToken = require("../middlewares/validateToken");
 
-productRouter.get("/productsList", productController.getAllProducts);
-productRouter.post("/newProduct", productController.createProduct);
-productRouter.post("/productUpdate/:id", productController.updateProductById);
-productRouter.delete("/productDelete/:id", productController.deleteProduct);
+productRouter.get("/productsList",verifyToken, productController.getAllProducts);
+productRouter.post("/newProduct",verifyToken, productController.createProduct);
+productRouter.post("/productUpdate/:id",verifyToken, productController.updateProductById);
+productRouter.delete("/productDelete/:id",verifyToken, productController.deleteProduct);
 
 module.exports = productRouter;
